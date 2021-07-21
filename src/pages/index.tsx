e9005771203f5e47f5b83cc7702f70b4b7e167c5
Guigalaverna <guigalaverna@gmail.com>
2021-07-21 18:17:04 -0300

    Get stripe product price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from '../styles/home.module.scss'
+import { stripe } from '../services/stripe'
+import { formatPrice } from '../utils/formatPrice'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +26,27 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="./avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const price = await stripe.prices.retrieve('price_1JFmNnCbRv0xB5Pldg7DojVg')
+
+  const product = {
+    priceId: price.id,
+    amount: formatPrice(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
