23deb63cd96367aa3dc130694a3db0fdfedc8b6e
Guigalaverna <guigalaverna@gmail.com>
2021-07-21 17:22:55 -0300

    Change button styles if user is authenticated

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
 import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+import styles from './styles.module.scss'
 
 export function SignInButton() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+
   return (  
-    <button>
-      <FaGithub />
-      Sign in with Github
+    <button
+      type='button'
+      className={styles.signInButton}
+    >
+      { isAuthenticated ? (
+        <>
+          <FaGithub color='#04D361'/>
+          Guilherme Galaverna
+          <FiX color='#737380' className={styles.closeIcon}/>
+        </>
+      ) : (
+        <>
+          <FaGithub color='#EBA417'/>
+          Sign in with Github
+        </>
+      ) }
     </button>
   )
 }
