92b6b6995a35ecb810fb4ad1c02f965f06bccc98
Guigalaverna <guigalaverna@gmail.com>
2021-07-22 01:01:30 -0300

    Change button styles if user is authenticated

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,29 +1,38 @@
-import { useState } from 'react'
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const [session] = useSession();
 
-  return (  
+  function handleSignIn() {
+    signIn("github");
+  }
+
+  function handleSignOut() {
+    signOut()
+  }
+
+  return !session ? (
     <button
-      type='button'
+      type="button"
       className={styles.signInButton}
+      onClick={handleSignIn}
     >
-      { isAuthenticated ? (
-        <>
-          <FaGithub color='#04D361'/>
-          Guilherme Galaverna
-          <FiX color='#737380' className={styles.closeIcon}/>
-        </>
-      ) : (
-        <>
-          <FaGithub color='#EBA417'/>
-          Sign in with Github
-        </>
-      ) }
+      <FaGithub color="#EBA417" />
+      Sign in with Github
     </button>
-  )
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={handleSignOut}
+    >
+      <FaGithub color="#04D361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  );
 }
