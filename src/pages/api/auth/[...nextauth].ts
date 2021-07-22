dc5d3c3fa70e5b9d0f6e2495c5a3f33a92a5ab9f
Guigalaverna <guigalaverna@gmail.com>
2021-07-22 02:17:02 -0300

    Dosen't register user if already exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,21 +14,23 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn(user, acc, profile) {
-      const { email } = user
+      const { email } = user;
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email }}
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
           )
-        )
-        
-        return true
-      } catch (err) {
-        return false
-      }
+        );
 
-    }
-  }
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+  },
 });
